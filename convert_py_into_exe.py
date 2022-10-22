e00eb3377256e7f5f6b4e37b104cfb4d4bd130c0
kundeshwar <110928245+kundeshwar@users.noreply.github.com>
2022-10-23 02:06:02 +0530

    Add files via upload

---
+++ convert_py_into_exe.py
@@ -0,0 +1,5 @@
+#convert .py file into .exe
+#to send your friends some time .py file is not sended so use exe file to send to someone
+#pyinstaller --onefile (name of file)
+# auto-py-to-exe model
+
